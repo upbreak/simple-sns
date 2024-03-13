3d1f6808ed5b63beadc6ca93c061dbd534711204
jinwoo <upbreak05@kakao.com>
2024-03-13 14:46:47 +0900

    init: initialize project

---
+++ src/test/java/com/jinwoo/dev/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinwoo.dev.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
