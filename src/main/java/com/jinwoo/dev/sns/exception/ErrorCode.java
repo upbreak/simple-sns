02e2a18c4c11a716ee63fbc4a2aad263b9b84c4a
jinwoo <upbreak05@kakao.com>
2024-03-14 12:07:05 +0900

    errorCode 추가 및 SnsApplicationException 생성자 추가

--- src/main/java/com/jinwoo/dev/sns/exception/ErrorCode.java
+++ src/main/java/com/jinwoo/dev/sns/exception/ErrorCode.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated")
+    , INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error")
+    , USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found")
+    , INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Invalid password")
     , TEMP(HttpStatus.NOT_FOUND, "temp");
 
     private HttpStatus status;
