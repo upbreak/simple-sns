c4d31913f444473c9002ef20b17044f33e6fdbbb
jinwoo <upbreak05@kakao.com>
2024-03-19 17:11:27 +0900

    알람 화면 수정
    sse기능을 사용하기 위하여 수정하였다

--- front-end/src/layouts/alarm/index.js
+++ front-end/src/layouts/alarm/index.js
@@ -61,8 +61,10 @@ function Alarm() {
   const [render, setRender] = useState(false);
   const [alarms, setAlarms] = useState([]);
   const [totalPage, setTotalPage] = useState(0);
+  const [alarmEvent, setAlarmEvent] = useState(undefined); /* set sse */
 
   const navigate = useNavigate();
+  let eventSource = undefined; /* set sse */
 
   const changePage = (pageNum) => {
     console.log('change pages');
@@ -75,7 +77,8 @@ function Alarm() {
   const handleGetAlarm = (pageNum, event) => {
     console.log('handleGetAlarm');
     axios({
-      url: '/api/v1/users/alarm?size=5&sort=id&page=' + pageNum,
+      // url: '/api/v1/users/alarm?size=5&sort=id&page=' + pageNum,
+      url: '/api/v1/users/alarm?size=5&sort=id,desc&page=' + pageNum,
       method: 'GET',
       headers: {
         Authorization: 'Bearer ' + localStorage.getItem('token'),
@@ -95,6 +98,29 @@ function Alarm() {
 
   useEffect(() => {
     handleGetAlarm();
+
+    /* set sse start */
+    eventSource = new EventSource("http://localhost:8080/api/v1/users/alarm/subscribe?token=" + localStorage.getItem('token'));
+
+    setAlarmEvent(eventSource);
+
+    eventSource.addEventListener("open", function (event) {
+      console.log("connection opened");
+    });
+
+    eventSource.addEventListener("alarm", function (event) {
+      console.log(event.data);
+      handleGetAlarm();
+    });
+
+    eventSource.addEventListener("error", function (event) {
+      console.log(event.target.readyState);
+      if (event.target.readyState === EventSource.CLOSED) {
+          console.log("eventsource closed (" + event.target.readyState + ")");
+      }
+      eventSource.close();
+    });
+    /* set sse end */
   }, []);
 
   return (
